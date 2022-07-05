24b0bfcc5a32c1980279c2dbb1906af2e670f868
Jesse landis <jesse.landis@ufl.edu>
2022-07-05 00:35:04 -0400

    Basics

--- App.js
+++ App.js
@@ -1,20 +1,128 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, TextInput, SafeAreaView} from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+import { useNavigation } from '@react-navigation/core'
+import KeyboardAvoidingView from 'react-native/Libraries/Components/Keyboard/KeyboardAvoidingView';
+import get_mp3 from main.py
 
+const handleSignOut = () => {
+  
+}
+const HomeScreen = () => {
+  const navigation = useNavigation()
+  const [text, onChangeText] = React.useState("");
+  const [number, onChangeNumber] = React.useState(null);
+  const hello = 'john';
+  return (
+    <KeyboardAvoidingView
+       style={styles.container}
+        behavior="padding">
+      <View style={styles.container}>
+        <Text>
+          Youtube to MP3
+        </Text>
+        <TextInput
+          placeholder="Put Your Youtube Link here!"
+          value={text}
+          onChangeText={onChangeText}
+          style={styles.input}
+        />
+      </View>
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={handleSignOut}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Get File</Text>
+      </TouchableOpacity>
+    </View>
+    
+  </KeyboardAvoidingView>
+)
+}/*
+const UselessTextInput = () => {
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [number, onChangeNumber] = React.useState(null);
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
+    </SafeAreaView>
+  );
+};
+*/
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 5,
+    height: 50,
+    paddingVertical: 5,
+    borderRadius: 5,
+    marginTop: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 120,
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782F9',
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: '#0782F9',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+})
